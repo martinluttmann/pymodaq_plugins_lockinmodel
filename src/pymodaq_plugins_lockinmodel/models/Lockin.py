e900cfdfb4b266fb0a5721a483966b75d8fe8b0b
martinluttmann <44998370+martinluttmann@users.noreply.github.com>
2026-01-23 15:18:32 +0100

    Started to implement a mock viewer to test the lockin

--- src/pymodaq_plugins_lockinmodel/models/Lockin.py
+++ src/pymodaq_plugins_lockinmodel/models/Lockin.py
@@ -6,7 +6,16 @@ from pymodaq_gui.parameter import Parameter
 
 
 class DataMixerTraining(DataMixerModel):
-    params = [{'title': 'Lock-in frequency', 'name':'lifreq', 'type':'float', 'value':500, 'suffix':'Hz', 'visible':True},]
+    params = [ {'title': 'Lockin prameters',
+         'name': 'Liparams',
+         'type': 'group', 'children':
+            [
+{'title': 'Frequency', 'name':'liFreq', 'type':'float', 'value':500, 'suffix':'Hz', 'visible':True},
+              {'title': 'Phase', 'name': 'liPhase', 'type': 'float', 'value': 0, 'suffix': 'rad', 'visible': True},
+              {'title': 'Time constant', 'name': 'liTime', 'type': 'float', 'value': 200, 'suffix': 'ms', 'visible': True},
+
+
+              ] } ]
     def ini_model(self):
         pass
 
@@ -14,6 +23,15 @@ class DataMixerTraining(DataMixerModel):
         if param.name() == 'get_data':
             pass
     def process_dte(self, dte: DataToExport):
+        trace = dte.get_data_from_name('MockSignalForLockin')[0]
 
 
-        pass
+        new_data = DataToExport('Lockin',
+                           data=[
+                               DataCalculated('divided by 2', data=[trace/2], labels=['Raw beam'])
+
+                           ]
+
+                           )
+
+        return new_data
