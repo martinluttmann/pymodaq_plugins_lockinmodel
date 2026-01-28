Impelmented lockin operation with correct units

src/pymodaq_plugins_lockinmodel/daq_viewer_plugins/plugins_1D/daq_1Dviewer_MockSignalForLockin.py
@@ -20,7 +20,7 @@ class DAQ_1DViewer_MockSignalForLockin(DAQ_Viewer_base):
          'tip': 'if true, plugin produces multiple curves (2) otherwise produces one curve with 2 peaks'},
         {'title': 'Mock1:', 'name': 'Mock1', 'type': 'group', 'children': [
             {'title': 'Amp:', 'name': 'Amp', 'type': 'int', 'value': 20, 'default': 20},
-            {'title': 'x0:', 'name': 'x0', 'type': 'float', 'value': 500, 'default': 500},
+            {'title': 'x0:', 'name': 'x0', 'type': 'float', 'value': 0, 'default': 0},
             {'title': 'dx:', 'name': 'dx', 'type': 'float', 'value': 0.3, 'default': 20},
             {'title': 'n:', 'name': 'n', 'type': 'int', 'value': 1, 'default': 1, 'min': 1},
             {'title': 'noise:', 'name': 'amp_noise', 'type': 'float', 'value': 0.1, 'default': 0.1, 'min': 0}
@@ -33,8 +33,8 @@ class DAQ_1DViewer_MockSignalForLockin(DAQ_Viewer_base):
             {'title': 'noise:', 'name': 'amp_noise', 'type': 'float', 'value': 0.1, 'default': 0.1, 'min': 0}, ]},
 
         {'title': 'xaxis:', 'name': 'x_axis', 'type': 'group', 'children': [
-            {'title': 'Npts:', 'name': 'Npts', 'type': 'int', 'value': 513, },
-            {'title': 'x0:', 'name': 'x0', 'type': 'float', 'value': 515, },
+            {'title': 'Npts:', 'name': 'Npts', 'type': 'int', 'value': 200000, },
+            {'title': 'x0:', 'name': 'x0', 'type': 'float', 'value': 200000, },
             {'title': 'dx:', 'name': 'dx', 'type': 'float', 'value': 0.1, },
         ]},
     ]
@@ -110,7 +110,7 @@ class DAQ_1DViewer_MockSignalForLockin(DAQ_Viewer_base):
         Npts = self.settings['x_axis', 'Npts']
         x0 = self.settings['x_axis', 'x0']
         dx = self.settings['x_axis', 'dx']
-        self.x_axis = Axis(label='photon wavelength', units='nm',
+        self.x_axis = Axis(label='Samples', units='samples',
                            data=linspace_step(x0 - (Npts - 1) * dx / 2, x0 + (Npts - 1) * dx / 2, dx),
                            index=0)
         self._update_x_axis = True
@@ -127,8 +127,8 @@ class DAQ_1DViewer_MockSignalForLockin(DAQ_Viewer_base):
 
         if self.is_master:
 
-            self.settings.child('x_axis', 'Npts').setValue(512)
-            self.settings.child('x_axis', 'x0').setValue(256)
+            self.settings.child('x_axis', 'Npts').setValue(100000)
+            self.settings.child('x_axis', 'x0').setValue(0)
             self.settings.child('x_axis', 'dx').setValue(1)
 
             self.settings.child('Mock1', 'x0').setValue(125)
@@ -202,10 +202,10 @@ class DAQ_1DViewer_MockSignalForLockin(DAQ_Viewer_base):
                                                                     axes=[self.x_axis])]))
             self._update_x_axis = False
         """
-        omega  = 0.1
+        frequency = 500 #Hz
         phase_noise = np.pi/8
         amp_noise = 10
-        data_tot = np.sin(self.x_axis.get_data() * omega + phase_noise* np.random.rand()) + amp_noise * np.random.rand((self.x_axis.size))
+        data_tot = np.sin(2*np.pi* self.x_axis.get_data()/1e6 * frequency + phase_noise* np.random.rand()) + amp_noise * np.random.rand((self.x_axis.size))
 
 
 

src/pymodaq_plugins_lockinmodel/models/Lockin.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy import signal
+
 from pymodaq.extensions.data_mixer.model import DataMixerModel
 
 from pymodaq_data.data import DataToExport, DataWithAxes, DataCalculated, DataDim
@@ -7,16 +9,14 @@ from pymodaq_gui.parameter import Parameter
 
 
 class DataMixerTraining(DataMixerModel):
-    params = [ {'title': 'Lockin prameters',
-         'name': 'Liparams',
-         'type': 'group', 'children':
-            [
-{'title': 'Frequency', 'name':'liFreq', 'type':'float', 'value':500, 'suffix':'Hz', 'visible':True},
+
+
+    params = [ {'title': 'Frequency', 'name':'liFreq', 'type':'float', 'value':500, 'suffix':'Hz', 'visible':True},
               {'title': 'Phase', 'name': 'liPhase', 'type': 'float', 'value': 0, 'suffix': 'rad', 'visible': True},
               {'title': 'Time constant', 'name': 'liTime', 'type': 'float', 'value': 200, 'suffix': 'ms', 'visible': True},
-
-
-              ] } ]
+            {'title': 'Sampling rate', 'name': 'SamplingRate', 'type': 'int', 'value': 1, 'suffix': 'MHz',
+                         'visible': True},
+               ]
     def ini_model(self):
         pass
 
@@ -26,12 +26,18 @@ class DataMixerTraining(DataMixerModel):
     def process_dte(self, dte: DataToExport):
         trace = dte.get_data_from_name('MockSignalForLockin')[0]
 
-        trace_multiplied = self.multiply_with_sine_wave(self, trace, trace.axes(), 1, 0)
+        print(self.settings.children())
+
+        reference_wave = self.square_wave(trace, self.settings.child('liFreq').value(), self.settings.child('liPhase').value())
+
+        trace_multiplied_integrated = self.multiply_with_ref_wave_and_integrate(trace, self.settings.child('liFreq').value(), self.settings.child('liPhase').value())
 
 
         new_data = DataToExport('Lockin',
                            data=[
-                               DataCalculated('multiplied', data=[trace_multiplied], labels=['Multiplied trace'])
+                               DataCalculated('Traces', data=[np.atleast_1d(trace), np.atleast_1d(reference_wave)],
+                                              labels=['raw signal', 'reference wave']),
+                               DataCalculated('multiplied', data=[np.atleast_1d(trace_multiplied_integrated)], labels=['Lockin signal'])
 
                            ]
 
@@ -40,8 +46,22 @@ class DataMixerTraining(DataMixerModel):
         return new_data
 
 
-    def multiply_with_sine_wave(self, data, axis, freq, phase):
+    def multiply_with_ref_wave_and_integrate(self, data, freq, phase):  #freq in acuqisition units, defined by user's sample rate
 
-        sine_wave = np.sin(axis*freq + phase)
+        Npts = np.shape(data)[0]
+        time_step = 1/(self.settings.child('SamplingRate').value()*1e6)
 
-        return np.multiply(data, sine_wave)
+        time_window = self.settings.child('liTime').value()/(Npts*time_step)
+        index_max = int(time_window * Npts)
+
+        sine_wave = self.square_wave(data, freq, phase)
+
+        return np.sum(np.multiply(data[:index_max], sine_wave[:index_max])) / index_max
+
+    def sine_wave(self, data, freq, phase):
+        axis = np.linspace(0, np.shape(data)[0] * 1 / (self.settings.child('SamplingRate').value()*1e6), np.shape(data)[0])
+        return np.sin(2*np.pi*axis*freq + phase)
+
+    def square_wave(self, data, freq, phase):
+        axis = np.linspace(0, np.shape(data)[0] * 1 / (self.settings.child('SamplingRate').value()*1e6), np.shape(data)[0])
+        return signal.square(t=2*np.pi*axis*freq + phase, duty=0.5)